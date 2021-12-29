dc42d220b2cc0e2efd2f766d4c60757b30bbd642
1139923280 <1139923280@qq.com>
2021-12-29 15:18:02 +0800

    2

--- 初阶/04-hanshu.py
+++ 初阶/04-hanshu.py
@@ -1,6 +1,13 @@
+# print()函数也可以接受多个字符串，用逗号“,”隔开，就可以连成一串输出：
+# 输入，name = input()并按下回车后，Python交互式命令行就在等待你的输入了。这时，你可以输入任意字符，然后按回车后完成输入。
+# name = input()
+# print(name)
+
 # 函数 len()、适用于变量、列表、
 s = '123456780sdjfosjfojsofdsof'
 print(len(s))
 
 liebiao = ['123', '456', '789']
 print(len(liebiao))
+
+
